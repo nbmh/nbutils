676f14d401261bac258d6c02dcb55a7dbc2eba70
Michał Hałas <michal.halas@zetosoftware.pl>
2017-05-09 13:00:11 +0200

    Update in cordova

--- dist/nbUtils.js
+++ dist/nbUtils.js
@@ -796,7 +796,7 @@
       restrict: ngIf.restrict,
       link: function ($scope, $element, $attr, ctrl, $transclude) {
         $attr.ngIf = function () {
-          return $cordova();
+          return $cordova.exists();
         };
         ngIf.link.call(ngIf, $scope, $element, $attr, ctrl, $transclude);
       }
@@ -806,7 +806,7 @@
     return {
       restrict: 'A',
       link: function ($scope, $element) {
-        if ($cordova()) {
+        if ($cordova.exists()) {
           $element.removeClass('ng-hide');
         } else {
           $element.addClass('ng-hide');
@@ -818,7 +818,7 @@
     return {
       restrict: 'A',
       link: function ($scope, $element) {
-        if ($cordova()) {
+        if ($cordova.exists()) {
           $element.addClass('ng-hide');
         } else {
           $element.removeClass('ng-hide');
@@ -831,15 +831,41 @@
     
     provider.$get = [function() {
       var service = function() {
-        return service.exists();
+        return service.exists() ? window['cordova'] : null;
       };
       
       service.exists = function() {
-        return true;//window['cordova'] != undefined;
+        return window['cordova'] != undefined;
       };
       
       service.is = function() {
         return service.exists();
+      };
+      
+      return service;
+    }];
+  }])
+  .provider('$plugins', [function() {
+    var provider = this;
+    
+    provider.$get = ['$cordova', function($cordova) {
+      var service = function(name) {
+        if (name && name != '') {
+          return service.get(name);
+        }
+        return service.cordova() ? $cordova().plugins : null;
+      };
+      
+      service.cordova = function() {
+        return $cordova.exists() && $cordova().plugins;
+      };
+      
+      service.exists = function(name) {
+        return service.cordova() && $cordova().plugins[name] != undefined;
+      };
+      
+      service.get = function(name) {
+        return service.exists(name) ? $cordova().plugins[name] : null;
       };
       
       return service;
--- src/cordova.js
+++ src/cordova.js
@@ -13,7 +13,7 @@
       restrict: ngIf.restrict,
       link: function ($scope, $element, $attr, ctrl, $transclude) {
         $attr.ngIf = function () {
-          return $cordova();
+          return $cordova.exists();
         };
         ngIf.link.call(ngIf, $scope, $element, $attr, ctrl, $transclude);
       }
@@ -23,7 +23,7 @@
     return {
       restrict: 'A',
       link: function ($scope, $element) {
-        if ($cordova()) {
+        if ($cordova.exists()) {
           $element.removeClass('ng-hide');
         } else {
           $element.addClass('ng-hide');
@@ -35,7 +35,7 @@
     return {
       restrict: 'A',
       link: function ($scope, $element) {
-        if ($cordova()) {
+        if ($cordova.exists()) {
           $element.addClass('ng-hide');
         } else {
           $element.removeClass('ng-hide');
@@ -48,15 +48,41 @@
     
     provider.$get = [function() {
       var service = function() {
-        return service.exists();
+        return service.exists() ? window['cordova'] : null;
       };
       
       service.exists = function() {
-        return true;//window['cordova'] != undefined;
+        return window['cordova'] != undefined;
       };
       
       service.is = function() {
         return service.exists();
+      };
+      
+      return service;
+    }];
+  }])
+  .provider('$plugins', [function() {
+    var provider = this;
+    
+    provider.$get = ['$cordova', function($cordova) {
+      var service = function(name) {
+        if (name && name != '') {
+          return service.get(name);
+        }
+        return service.cordova() ? $cordova().plugins : null;
+      };
+      
+      service.cordova = function() {
+        return $cordova.exists() && $cordova().plugins;
+      };
+      
+      service.exists = function(name) {
+        return service.cordova() && $cordova().plugins[name] != undefined;
+      };
+      
+      service.get = function(name) {
+        return service.exists(name) ? $cordova().plugins[name] : null;
       };
       
       return service;
