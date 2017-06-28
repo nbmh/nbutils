3825c99bc9c235421352ebd3f8c1a6d07fbde017
Michał Hałas <michal.halas@zetosoftware.pl>
2017-06-28 11:28:04 +0200

    Back button logic change

--- src/back-button.js
+++ src/back-button.js
@@ -7,52 +7,64 @@
   angular.module('nb.backbutton', [
     'ui.router'
   ])
-  .directive('nbBackButton', ['$rootScope', '$state', '$transitions', function($rootScope, $state, $transitions) {
+  .directive('nbBackButton', ['$rootScope', '$state', '$transitions', '$timeout', function($rootScope, $state, $transitions, $timeout) {
     return {
       restrict: 'EA',
       transclude: true,
       scope: {
-        state: '@',
-        params: '='
+        name: '@',
+        state: '='
       },
       link: function($scope, $element, $attr, ctrl, transclude) {
         $element.addClass('nb-back-button').addClass('ng-hide');
         
-        var state = null,
-        params = {};
+        var main = false;
         
         transclude($scope, function(transEl) {
           $element.prepend(transEl);
         });
         
-        if ($scope.state) {
-          $rootScope.$emit('$nb.backbutton', $scope.state, $scope.params);
+        if ($scope.name || $scope.state) {
+          var eventGet = $rootScope.$on('$nb.backbutton.get', function() {
+            $rootScope.$emit('$nb.backbutton.give', {name: $scope.name, state: $scope.state});
+          });
+          $rootScope.$emit('$nb.backbutton', null);
+          
+          $scope.$on('$destroy', function() {
+            eventGet();
+          });
         } else {
           $scope.$back = function() {
-            $rootScope.$emit('$nb.backbutton', null);
-            $state.go(state, params);
+            $rootScope.$emit('$nb.backbutton.get');
           };
-          $rootScope.$on('$nb.backbutton', function(e, s, p) {
-            if (s) {
-              state = s;
-              params = p || {};
-              $element.removeClass('ng-hide');
+          var eventGive = $rootScope.$on('$nb.backbutton.give', function(e, data) {
+            if (data.state) {
+              $state.go(data.state.state, data.state.params || {}, data.state.options || {});
+            } else {
+              $state.go(data.name);
             }
           });
+          var event = $rootScope.$on('$nb.backbutton', function() {
+            main = true;
+            $element.removeClass('ng-hide');
+          });
+          $scope.$on('$destroy', function() {
+            eventGive();
+            event();
+          });
           $transitions.onStart({}, function() {
-            state = null;
-            params = {};
+            main = false;
           });
           $transitions.onSuccess({}, function() {
-            setTimeout(function() {
-              if (!state) {
+            $timeout(function() {
+              if (!main) {
                 $element.addClass('ng-hide');
               }
             }, 10);
           });
           
           setTimeout(function() {
-            if (!state || state == null) {
+            if (!main) {
               $element.addClass('ng-hide');
             }
           }, 10);
